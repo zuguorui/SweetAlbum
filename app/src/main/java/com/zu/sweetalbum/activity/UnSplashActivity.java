7bbcac97f5ea24f1c0673069918750876a14782f
zuguorui <zu_guorui@126.com>
2017-09-06 23:21:57 +0800

    completing unsplash activity

--- app/src/main/java/com/zu/sweetalbum/activity/UnSplashActivity.java
+++ app/src/main/java/com/zu/sweetalbum/activity/UnSplashActivity.java
@@ -3,18 +3,23 @@ package com.zu.sweetalbum.activity;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 
 import com.zu.sweetalbum.R;
+import com.zu.sweetalbum.util.CommonUtil;
 import com.zu.sweetalbum.view.TextViewPagerIndicator;
 
-public class UnSplashActivity extends AppCompatActivity {
+public class UnSplashActivity extends AppCompatActivity implements View.OnClickListener{
     private ViewPager mViewPager;
     private FrameLayout customActionBarArea;
     private ViewGroup actionBarContainer;
     private ImageView slideIndicator;
+    private ImageView refreshButton;
+    private ImageView searchButton;
+
 
     private TextViewPagerIndicator viewPagerIndicator;
     private ViewGroup actionBar;
@@ -28,10 +33,36 @@ public class UnSplashActivity extends AppCompatActivity {
     private void initViews()
     {
         actionBarContainer = (ViewGroup)findViewById(R.id.UNSPLASHActivity_ActionBar);
-        customActionBarArea = (FrameLayout)actionBar.findViewById(R.id.MainActivity_actionBar_custom_area);
-        slideIndicator = (ImageView)actionBar.findViewById(R.id.MainActivity_ActionBar_slideIndicator);
+        customActionBarArea = (FrameLayout)actionBarContainer.findViewById(R.id.MainActivity_actionBar_custom_area);
+        slideIndicator = (ImageView)actionBarContainer.findViewById(R.id.MainActivity_ActionBar_slideIndicator);
+        slideIndicator.setOnClickListener(this);
+        actionBar = (ViewGroup) getLayoutInflater().inflate(R.layout.unsplash_action_bar, null);
+        customActionBarArea.addView(actionBar);
+        refreshButton = (ImageView)actionBar.findViewById(R.id.UnSplashActionBar_imageButton_refresh);
+        refreshButton.setOnClickListener(this);
+        searchButton = (ImageView)actionBar.findViewById(R.id.UnSplashActionBar_imageButton_search);
+        searchButton.setOnClickListener(this);
 
         mViewPager = (ViewPager) findViewById(R.id.UNSPLASHActivity_ViewPager);
+        viewPagerIndicator = (TextViewPagerIndicator)findViewById(R.id.UNSPLASHActivity_viewPagerIndicator);
+
+        ViewGroup.MarginLayoutParams layoutParams = (ViewGroup.MarginLayoutParams) actionBarContainer.getLayoutParams();
+        int statusBarHeight = CommonUtil.getStatusBarHeight();
+        layoutParams.height += statusBarHeight;
+        actionBarContainer.setLayoutParams(layoutParams);
+        actionBarContainer.setPadding(actionBarContainer.getPaddingLeft(), actionBarContainer.getPaddingTop() + statusBarHeight,
+                actionBarContainer.getPaddingRight(), actionBarContainer.getPaddingBottom());
+
+        layoutParams = (ViewGroup.MarginLayoutParams) viewPagerIndicator.getLayoutParams();
+        layoutParams.topMargin = -layoutParams.height;
+
+
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
 
     }
 }
