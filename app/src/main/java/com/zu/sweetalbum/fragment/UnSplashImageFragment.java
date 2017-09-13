49001b1e5bc5901dcd9ecbee994fe9b2f61788b2
zuguorui <zu_guorui@126.com>
2017-09-13 23:03:36 +0800

    completing

--- app/src/main/java/com/zu/sweetalbum/fragment/UnSplashImageFragment.java
+++ app/src/main/java/com/zu/sweetalbum/fragment/UnSplashImageFragment.java
@@ -3,15 +3,24 @@ package com.zu.sweetalbum.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.zu.sweetalbum.R;
+import com.zu.sweetalbum.view.AlbumListView.ImageNoGroupedAdapter;
+import com.zu.sweetalbum.view.AlbumListView.ZoomLayoutManager;
 
 /**
  * Created by zu on 17-9-8.
  */
 
 public class UnSplashImageFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+    private ZoomLayoutManager zoomLayoutManager;
+    private ImageNoGroupedAdapter adapter;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +29,14 @@ public class UnSplashImageFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_unsplash_image, null);
+        initViews(root);
         return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    private void initViews(View root)
+    {
+        recyclerView = (RecyclerView)root.findViewById(R.id.UnSplashImageFragment_recyclerView);
+
     }
 }
