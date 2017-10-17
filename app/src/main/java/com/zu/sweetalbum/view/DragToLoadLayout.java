1e2b4d562ad71d9b17263994ab88de956e9ec2a6
zuguorui <zu_guorui@126.com>
2017-10-17 23:10:25 +0800

    drag to load

--- app/src/main/java/com/zu/sweetalbum/view/DragToLoadLayout.java
+++ app/src/main/java/com/zu/sweetalbum/view/DragToLoadLayout.java
@@ -1,6 +1,7 @@
 package com.zu.sweetalbum.view;
 
 import android.content.Context;
+import android.graphics.Rect;
 import android.support.annotation.AttrRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -9,26 +10,65 @@ import android.util.AttributeSet;
 import android.view.View;
 import android.widget.FrameLayout;
 
+import com.zu.sweetalbum.view.AlbumListView.DragLoadView;
+
 /**
  * Created by zu on 2017/10/17.
  */
 
 public class DragToLoadLayout extends FrameLayout{
+
+    private DragLoadView upDragLoadView;
+    private DragLoadView downDragLoadView;
     public DragToLoadLayout(@NonNull Context context) {
-        super(context);
+        this(context, null);
     }
 
     public DragToLoadLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs, 0);
     }
 
     public DragToLoadLayout(@NonNull Context context, @Nullable AttributeSet attrs, @AttrRes int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
+        this(context, attrs, defStyleAttr, 0);
+
     }
 
     public DragToLoadLayout(@NonNull Context context, @Nullable AttributeSet attrs, @AttrRes int defStyleAttr, @StyleRes int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+        if(getChildCount() != 3)
+        {
+            throw new IllegalStateException("you must add 3 views to this DragLayout. The first view will be header and the last will be footer");
+        }
+
+        if(!(getChildAt(0) instanceof DragLoadView) || !(getChildAt(2) instanceof DragLoadView))
+        {
+            throw new IllegalStateException("the first and last child view should be DragLoadView");
+        }
+
+        upDragLoadView = (DragLoadView)getChildAt(0);
+        downDragLoadView = (DragLoadView)getChildAt(2);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        if(changed)
+        {
+            Rect rect = getVisibleRect();
+            upDragLoadView.layout(rect.left, rect.top - upDragLoadView.getMeasuredHeight(), rect.right, rect.top);
+            downDragLoadView.layout(rect.left, rect.bottom, rect.right, rect.bottom + downDragLoadView.getMeasuredHeight());
+            getChildAt(1).layout(rect.left, rect.top, rect.right, rect.bottom);
+        }
     }
 
 
+
+    private Rect getVisibleRect()
+    {
+        int top = getPaddingTop();
+        int left = getPaddingLeft();
+        int right = getWidth() - getPaddingRight();
+        int bottom = getHeight() - getPaddingBottom();
+        Rect rect = new Rect(left, top, right, bottom);
+        return rect;
+    }
 }
